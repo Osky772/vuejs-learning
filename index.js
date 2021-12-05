d48f9cf0fcda69f728177bffd79330312275b41c
Oskar Kowalów <kowalow.oskar@gmail.com>
2021-12-05 09:54:52 +0100

    binding data

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 new Vue({
     el: "#app",
     data: {
-        name: "Oskar"
+        name: "Oskar",
+        website: "https://google.com",
+        websiteTag: `<a href="https://google.com">My new link</a>`
     },
     methods: {
         greet() {
